7e616efeb572cb7a1d6360b872f5f8194510901e
Aditya Galande <adityagalande14@gmail.com>
2021-03-31 16:55:04 +0530

    initial commit

---
+++ app/src/main/java/com/covidupdate/android/MainActivity.java
@@ -0,0 +1,21 @@
+package com.covidupdate.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try {
+            Objects.requireNonNull(this.getSupportActionBar()).hide();
+        } catch (NullPointerException ignored) {
+        }
+    }
+}
