5e55820d271372dcd9990ef27a3390ac92fb1f61
Aditya Galande <adityagalande14@gmail.com>
2021-04-02 23:57:29 +0530

    suggestion code added

--- app/src/main/java/com/covidupdate/android/Services/Suggestions.java
+++ app/src/main/java/com/covidupdate/android/Services/Suggestions.java
@@ -3,6 +3,7 @@ package com.covidupdate.android.Services;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.covidupdate.android.R;
 
@@ -19,5 +20,11 @@ public class Suggestions extends AppCompatActivity {
             Objects.requireNonNull(this.getSupportActionBar()).hide();
         } catch (NullPointerException ignored) {
         }
+
+        TextView title2TextView = findViewById(R.id.suggestionFormTitle2);
+        String title2String = "Do you have suggestion or found some bug?\n" +
+                "let us know in the field below.";
+        title2TextView.setText(title2String);
+
     }
 }
