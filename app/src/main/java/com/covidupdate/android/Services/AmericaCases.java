b42fb75fb15cb7851efa00a9888f68249a947b84
Aditya Galande <adityagalande14@gmail.com>
2021-04-06 23:19:52 +0530

    new activities added

---
+++ app/src/main/java/com/covidupdate/android/Services/AmericaCases.java
@@ -0,0 +1,16 @@
+package com.covidupdate.android.Services;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.covidupdate.android.R;
+
+public class AmericaCases extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_america_cases);
+    }
+}
---
+++ app/src/main/java/com/covidupdate/android/Services/IndiaCases.java
@@ -0,0 +1,16 @@
+package com.covidupdate.android.Services;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.covidupdate.android.R;
+
+public class IndiaCases extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_india_cases);
+    }
+}
