d538483514bc22f95d29991ccc19d0f6457f555b
Aditya Galande <adityagalande14@gmail.com>
2021-11-03 21:29:07 +0530

    package changes

--- app/src/main/java/com/daily_cases_reports/android/Services/AboutUs.java
+++ app/src/main/java/com/daily_cases_reports/android/Services/AboutUs.java
@@ -19,13 +19,13 @@ public class AboutUs extends AppCompatActivity {
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.aboutus_action_bar);
 
-        content = "\n\nThe content of this App is for information purposes. It gives real-time COVID-19 cases update worldwide, India, The United States. Our motto is to make awareness in our society about COVID-19 pandamic. \n" +
+        content = "\n\nThe content of this App is for information purposes. It gives real-time cases update worldwide, India, The United States. Our motto is to make awareness in our society about pandamic. \n" +
                 "\n" +
                 "\n" +
                 "Most of the advisory content is from the World Health Organization (WHO), Ministry of health and family welfare and Indian Council of Medical Research (ICMR).\n" +
                 "\n" +
                 "\n" +
-                "We are issue this App and it's content fighting against the pandemic of COVID-19. All advisory information publish through this App is guidelines only. App users are advised to follow local rules and regulations, Health safety requirement of respective authority on any  issue dealing with COVID-19 and their response.\n" +
+                "We are issue this App and it's content fighting against the pandemic. All advisory information publish through this App is guidelines only. App users are advised to follow local rules and regulations, Health safety requirement of respective authority on any  issue dealing with virus and their response.\n" +
                 "\n" +
                 "\n" +
                 "The creator of this App nor the publication platform will not be responsible for any unforeseen circumstances for the same.\n"+
